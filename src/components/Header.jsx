661f4d9e3895f34f4e5e7d2f4c3a96a79b6dfbe9
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-25 10:05:49 +0100

    fai Header.jsx (import)

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+// importo dalla libreria installata react-router-dom per la navigazione
+
+import {Link, NavLink } from "react-router-dom";
+
+export default function Header () {
+    return (
+        <header>
+            <div className="nav-bar">
+                {/* NavLink */}
+                <NavLink to="/"></NavLink>
+                <NavLink to="/chisiamo"></NavLink>
+                <NavLink to="/listpost">Lista dei post</NavLink>
+            </div>
+        </header>
+    )
+}
